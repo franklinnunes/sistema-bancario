01b1548278edd94f4e7c4f5915ac605b1a55671a
Franklin <franklinryu@gmail.com>
2023-09-05 10:19:50 -0300

    2nd commit

--- src/routes.js
+++ src/routes.js
@@ -1,35 +1,27 @@
 const express = require('express');
-const { listarContas } = require('./controladores/consultas/listarContasBancarias');
+const { listarContas } = require('./controllers/consultas/listarContasBancarias');
 const { validarSenha } = require('./middlewares/checarSenha');
-const { criarConta } = require('./controladores/gerenciamento/criarContaBancaria');
-const { atualizarUsuario } = require('./controladores/gerenciamento/atualizarUsuario');
-const { excluirConta } = require('./controladores/gerenciamento/excluirConta');
-const { depositar } = require('./controladores/movimentacoes/depositar');
-const { sacar } = require('./controladores/movimentacoes/sacar');
-const { transferir } = require('./controladores/movimentacoes/transferir');
-const { consultarSaldo } = require('./controladores/consultas/consultarSaldo');
-const { listarTransacoes } = require('./controladores/consultas/extrato');
+const { criarConta } = require('./controllers/gerenciamento/criarContaBancaria');
+const { atualizarUsuario } = require('./controllers/gerenciamento/atualizarUsuario');
+const { excluirConta } = require('./controllers/gerenciamento/excluirConta');
+const { depositar } = require('./controllers/movimentacoes/depositar');
+const { sacar } = require('./controllers/movimentacoes/sacar');
+const { transferir } = require('./controllers/movimentacoes/transferir');
+const { consultarSaldo } = require('./controllers/consultas/consultarSaldo');
+const { listarTransacoes } = require('./controllers/consultas/extrato');
 
 const router = express();
 
 router.use(express.json());
 
 router.get('/contas', validarSenha, listarContas);
-
 router.post('/contas', criarConta);
-
 router.put('/contas/:numeroConta/usuario', atualizarUsuario);
-
 router.delete('/contas/:numeroConta', excluirConta);
-
 router.post('/transacoes/depositar', depositar);
-
 router.post('/transacoes/sacar', sacar);
-
 router.post('/transacoes/transferir', transferir);
-
 router.get('/contas/saldo', consultarSaldo);
-
 router.get('/contas/extrato', listarTransacoes);
 
 module.exports = router;
